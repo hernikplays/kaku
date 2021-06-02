55b5575d773c3aee11aabc1ff0d592b422192434
Hernik <hernik27@gmail.com>
2021-06-02 19:13:57 +0200

    Start making drawing practice(?)

---
+++ app/src/main/java/cz/hernik/kaku/helper/DrawView.java
@@ -0,0 +1,13 @@
+package cz.hernik.kaku.helper;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class DrawView extends View {
+    public DrawView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
